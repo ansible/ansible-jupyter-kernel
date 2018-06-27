6bcdaf6eb1aef72776db60391bae3a5ec3ef2c05
Matthew Jones <bsdmatburt@gmail.com>
2018-06-27 12:41:47 -0400

    Filling out README and expanding the setup utility
    This also includes a setup.cfg to aid linting tools

--- setup.py
+++ setup.py
@@ -1,12 +1,48 @@
+import json
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class Installer(install):
+    def run(self):
+        # Regular install
+        install.run(self)
+
+        # Post install
+        print('Installing Ansible Kernel kernelspec')
+        from jupyter_client.kernelspec import KernelSpecManager
+        from IPython.utils.tempdir import TemporaryDirectory
+        kernel_json = {
+            "argv": ["python", "-m", "ansible_kernel", "-f", "{connection_file}"],
+            "codemirror_mode": "yaml",
+            "display_name": "Ansible",
+            "language": "ansible"
+        }
+        with TemporaryDirectory() as td:
+            os.chmod(td, 0o755)
+            with open(os.path.join(td, 'kernel.json'), 'w') as f:
+                json.dump(kernel_json, f, sort_keys=True)
+            ksm = KernelSpecManager()
+            ksm.install_kernel_spec(td, 'ansible', user=self.user, replace=True, prefix=self.prefix)
+
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
-    name='ansible_kernel',
-    version='0.0.1',
+    name='ansible-kernel',
+    version='0.1',
+    description='An Ansible kernel for Jupyter',
+    long_description=long_description,
     packages=find_packages(),
+    cmdclass={'install': Installer},
+    license='Apache',
     install_requires=[
         'PyYAML',
         'psutil',
+        'jupyter',
     ],
     zip_safe=False
 )
