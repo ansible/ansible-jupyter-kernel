30563f5a2f748ef8f05fbfc233b83eebb4a4b9c2
Ben Thomasson <bthomass@redhat.com>
2018-07-17 13:09:07 -0400

    Improve logging format

--- ansible_kernel/__main__.py
+++ ansible_kernel/__main__.py
@@ -1,5 +1,6 @@
 import logging
-logging.basicConfig(filename='ansible_kernel.log', level=logging.DEBUG) # noqa
+FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+logging.basicConfig(filename='ansible_kernel.log', level=logging.DEBUG, format=FORMAT) # noqa
 from ipykernel.kernelapp import IPKernelApp
 from .kernel import AnsibleKernel
 IPKernelApp.launch_instance(kernel_class=AnsibleKernel)
